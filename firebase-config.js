8227218737bc2efb35feab75b0a272b7ac6ff45b
nxhettry <nishan.gtsn@gmail.com>
2025-03-22 12:39:55 +0545

    created changes in  the confiurations

--- firebase-config.js
+++ firebase-config.js
@@ -7,15 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBQVje7YgqdTkxrQfSCkc_4UnhjzqVjm6E",
-  authDomain: "keeper-10e79.firebaseapp.com",
-  databaseURL:
-    "https://keeper-10e79-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "keeper-10e79",
-  storageBucket: "keeper-10e79.firebasestorage.app",
-  messagingSenderId: "413220592847",
-  appId: "1:413220592847:web:74ab15da06364a86f8abba",
-  measurementId: "G-T4BYE7J300",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
